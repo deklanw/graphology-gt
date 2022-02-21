c2971ebd6d1d0c1ec898cb3e4ec045346e6d9561
Deklan Webster <deklanw@gmail.com>
2022-02-21 11:19:24 -0500

    switch to using microbundle

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+// don't conflict with microbundle's babel config when building
+module.exports = process.env.NODE_ENV == 'test' ? {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
   ],
-};
+} : {};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  modulePathIgnorePatterns: ["<rootDir>/lib/"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
